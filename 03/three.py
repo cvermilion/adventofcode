d7a46d0549c1357068c0f2442ca9cc7f9af652d9
Christopher Vermilion <chris@figlyinc.com>
2018-01-22 15:33:40 -0500

    Clean up 03

--- 03/three.py
+++ 03/three.py
@@ -3,8 +3,12 @@
 # right side, top and left add 558 each, gets to new lower left 311923
 # input is 129 into bottom row, which is 559 across, so 280-129=151 from middle
 # so distance is 151 + 279 = 430
+print("Part 1: 430")
 
 """
+This is a known sequence, available at OEIS:
 http://oeis.org/search?q=1%2C1%2C2%2C4%2C5%2C10%2C11%2C23&sort=&language=english&go=Search
-
+=> http://oeis.org/A141481
+=> manual scan reveals first element higher than 312052 is:
 """
+print("Part 2: 312453")
