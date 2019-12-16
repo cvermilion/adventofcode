2fbe951fffc6bcde6ec0deb273d2195b9eb5e5f6
Christopher Vermilion <chris@figlyinc.com>
2019-12-16 13:35:21 -0500

    2019 day03 part2

--- 2019/day03/src/main.rs
+++ 2019/day03/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::HashMap;
 use std::fs;
 
 enum Step {
@@ -17,46 +17,56 @@ fn str_of_step(s: &Step) -> String {
     }
 }
 
-// TODO: write as safe
-unsafe fn steps(line: String) -> Vec<Step> {
+fn steps(line: String) -> Vec<Step> {
     let parts = line.split(',');
     return parts
         .map(|x| {
             if x.len() < 2 {
                 return Step::R(0);
             }
-            let first = x.chars().next();
-            let num: isize = x.get_unchecked(1..x.len()).parse().expect("bad number!");
+            let first = &x[..1];
+            let num: isize = x[1..].parse().expect("bad number!");
             match first {
-                Some('R') => Step::R(num),
-                Some('L') => Step::L(num),
-                Some('U') => Step::U(num),
-                Some('D') => Step::D(num),
+                "R" => Step::R(num),
+                "L" => Step::L(num),
+                "U" => Step::U(num),
+                "D" => Step::D(num),
                 _ => Step::R(0),
             }
         })
         .collect();
 }
 
-fn do_n(n: isize, pts: &mut HashSet<[isize; 2]>, pt: &mut [isize; 2], dx: isize, dy: isize) {
+fn do_n(
+    n: isize,
+    cur_dist: &mut isize,
+    pts: &mut HashMap<[isize; 2], isize>,
+    pt: &mut [isize; 2],
+    dx: isize,
+    dy: isize,
+) {
     let mut i = 0;
     while i < n {
         i += 1;
+        *cur_dist += 1;
         pt[0] += dx;
         pt[1] += dy;
-        pts.insert(*pt);
+        if !pts.contains_key(pt) {
+            pts.insert(*pt, *cur_dist);
+        }
     }
 }
 
-fn points(steps: &Vec<Step>) -> HashSet<[isize; 2]> {
-    let mut pts: HashSet<[isize; 2]> = HashSet::new();
+fn points(steps: &Vec<Step>) -> HashMap<[isize; 2], isize> {
+    let mut pts: HashMap<[isize; 2], isize> = HashMap::new();
     let mut cur = [0, 0];
+    let mut dist = 0;
     for step in steps {
         match step {
-            Step::R(num) => do_n(*num, &mut pts, &mut (cur), 1, 0),
-            Step::L(num) => do_n(*num, &mut pts, &mut (cur), -1, 0),
-            Step::U(num) => do_n(*num, &mut pts, &mut (cur), 0, 1),
-            Step::D(num) => do_n(*num, &mut pts, &mut (cur), 0, -1),
+            Step::R(num) => do_n(*num, &mut dist, &mut pts, &mut (cur), 1, 0),
+            Step::L(num) => do_n(*num, &mut dist, &mut pts, &mut (cur), -1, 0),
+            Step::U(num) => do_n(*num, &mut dist, &mut pts, &mut (cur), 0, 1),
+            Step::D(num) => do_n(*num, &mut dist, &mut pts, &mut (cur), 0, -1),
         }
     }
     pts
@@ -68,26 +78,39 @@ fn main() {
     let l1 = lines[0];
     let l2 = lines[1];
 
-    unsafe {
-        let steps1 = steps(l1.to_string());
-        let steps2 = steps(l2.to_string());
+    let steps1 = steps(l1.to_string());
+    let steps2 = steps(l2.to_string());
 
-        let points1 = points(&steps1);
-        let points2 = points(&steps2);
+    let points1 = points(&steps1);
+    let points2 = points(&steps2);
 
-        let mut intxs: Vec<[isize; 2]> = Vec::new();
-        for pt in points2 {
-            if points1.contains(&pt) {
-                intxs.push(pt);
-            }
+    // part 1
+    let mut intxs: Vec<[isize; 2]> = Vec::new();
+    for (&pt, &dist) in &points2 {
+        if points1.contains_key(&pt) {
+            intxs.push(pt);
         }
-
-        let dists = intxs.iter().map(|pt| pt[0].abs() + pt[1].abs());
-        let mut min = 0;
-        if let Some(d) = dists.min() {
-            min = d;
-        }
-
-        println!("part 1: min distance to an intersection is {}", min);
     }
+
+    let dists = intxs.iter().map(|pt| pt[0].abs() + pt[1].abs());
+    let mut min = 0;
+    if let Some(d) = dists.min() {
+        min = d;
+    }
+
+    println!("part 1: min distance to an intersection is {}", min);
+
+    // part 2
+    let mut combined_steps = Vec::new();
+    for (&pt, &dist2) in &points2 {
+        if let Some(dist1) = points1.get(&pt) {
+            combined_steps.push(dist1 + dist2);
+        }
+    }
+
+    min = 0;
+    if let Some(d) = combined_steps.iter().min() {
+        min = *d;
+    }
+    println!("part 2: min combined steps to an intersection is {}", min);
 }
