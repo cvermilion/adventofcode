9b03195d40bedcf16010fdb982af3f9a0b6c7a61
Christopher Vermilion <christopher.vermilion@gmail.com>
2023-01-10 13:21:44 -0500

    clean up day 22

--- 2022/22/run2.py
+++ 2022/22/run2.py
@@ -1,25 +1,5 @@
 from parse import parse
 
-input_test = """        ...#
-        .#..
-        #...
-        ....
-...#.......#
-........#...
-..#....#....
-..........#.
-        ...#....
-        .....#..
-        .#......
-        ......#.
-
-10R5L5R10L4R5L5"""
-
-input = input_test
-N = 4
-W = 4*N
-H = 3*N
-
 input = open("input.py").read()
 N = 50
 W = 3*N
@@ -29,14 +9,8 @@ grid_text, steps_text = input.split("\n\n")
 
 grid = list(grid_text.splitlines())
 
-"""
-print(H)
-for row in grid:
-	print(len(row))
-"""
-	
-
 steps = []
+steps_text = steps_text.strip()
 while steps_text:
 	r = parse("{:d}{}", steps_text)
 	if not r:
@@ -61,10 +35,7 @@ class Cell (object):
 
 pts = [[(Cell(i,j,c) if (c != " ") else None) for (i,c) in enumerate(row)] for (j,row) in enumerate(grid)]
 
-#pts = [(Cell(i,j,c) if (c != " ") else None) for (i,c) in enumerate([])]
-
-# to do: compute rotations at the boundaries
-# 5 internal boundaries, no turn, 7 ext ones have a rot
+# initial connections (assumes wrapping, haven't cleaned that up)
 for j, row in enumerate(pts):
 	first = None
 	for i, cell in enumerate(row):
@@ -99,94 +70,6 @@ for i in range(W):
 		nxt.up = (cell, 0)
 		cell.down = (nxt, 0)
 		
-rot_map = {
-		"R": {"R": "D", "D": "L", "L": "U", "U": "R"},
-		"L": {"R": "U", "U": "L", "L": "D", "D": "R"}
-	}
-
-"""
-Square labeling in test data:
-	
-	A
-BCD
-  EF
-
-boundaries are AB, AC, AF, BE, BF, CE, DF
-"""
-
-"""
-# AB
-jA = 0
-jB = N
-for iA in range(2*N, 3*N):
-	iB = 3*N - 1 - iA
-	cellA = pts[jA][iA]
-	cellB = pts[jB][iB]
-	cellA.up = (cellB, 2)
-	cellB.up = (cellA, 2)
-
-# AC
-iA = 2*N
-jC = N
-for jA in range(N):
-	iC = N + jA
-	cellA = pts[jA][iA]
-	cellC = pts[jC][iC]
-	cellA.left = (cellC, 3)
-	cellC.up = (cellA, 1)
-
-# AF
-iA = 3*N-1
-iF = 4*N-1
-for jA in range(N):
-	jF = 3*N - 1 - jA
-	cellA = pts[jA][iA]
-	cellF = pts[jF][iF]
-	cellA.right = (cellF, 2)
-	cellF.right = (cellA, 2)
-
-# BE
-jB = 2*N-1
-jE = 3*N-1
-for iB in range(N):
-	iE = 3*N - 1 - iB
-	cellB = pts[jB][iB]
-	cellE = pts[jE][iE]
-	cellB.down = (cellE, 2)
-	cellE.down = (cellB, 2)
-
-# BF
-iB = 0
-jF = 3*N-1
-for jB in range(N, 2*N):
-	iF = 5*N - 1 - jB
-	cellB = pts[jB][iB]
-	cellF = pts[jF][iF]
-	cellB.left = (cellF, 1)
-	cellF.down = (cellB, 3)
-
-# CE
-jC = 2*N-1
-iE = 2*N
-for iC in range(N, 2*N):
-	jE = 4*N - 1 - iC
-	cellC = pts[jC][iC]
-	cellE = pts[jE][iE]
-	cellC.down = (cellE, 3)
-	cellE.left = (cellC, 1)
-
-# DF
-iD = 3*N-1
-jF = 2*N
-for jD in range(N, 2*N):
-	iF = 5*N - 1 - jD
-	cellD = pts[jD][iD]
-	cellF = pts[jF][iF]
-	cellD.right = (cellF, 1)
-	cellF.up = (cellD, 3)
-
-"""
-
 """
 Square labeling in real data:
 	
@@ -269,6 +152,11 @@ for iE in range(N, 2*N):
 	cellF.right = (cellE, 3)
 	
 	
+rot_map = {
+		"R": {"R": "D", "D": "L", "L": "U", "U": "R"},
+		"L": {"R": "U", "U": "L", "L": "D", "D": "R"}
+	}
+
 def do_step(cur, step):
 	n, turn = step
 	cell, dir = cur
@@ -279,7 +167,6 @@ def do_step(cur, step):
 		"D": lambda c: c.down
 	}
 	for _ in range(n):
-		print(cell.x, cell.y, "step", dir)
 		next_cell, rots = nxt[dir](cell)
 		if next_cell.is_wall:
 			break
@@ -290,6 +177,7 @@ def do_step(cur, step):
 	new_dir = rot_map[turn][dir]
 	return cell, new_dir
 
+# find upper left cell
 cell = None
 for c in pts[0]:
 	if c and not c.is_wall:
@@ -298,11 +186,9 @@ for c in pts[0]:
 
 dir = "R"
 for step in steps:
-	print(cell.x, cell.y, dir, step)
 	cell, dir = do_step((cell, dir), step)
 	
 score = 1000*(cell.y+1) + 4*(cell.x+1) + {"R":0, "D": 1, "L": 2, "U": 3}[dir]
 
-print(score)
+print("Part 2:", score)
 
-# too low: 109164
--- 2022/22/run2_test.py
+++ 2022/22/run2_test.py
@@ -15,23 +15,16 @@ input_test = """        ...#
 
 10R5L5R10L4R5L5"""
 
-test = False
-if test:
-    input = input_test
-    N = 4 
-    W = 4*N
-    H = 3*N
-else:
-    input = open("input.py").read()
-    N = 50
-    W = 3*N
-    H = 4*N
+input = input_test
+N = 4
+W = 4*N
+H = 3*N
 
 grid_text, steps_text = input.split("\n\n")
+
 grid = list(grid_text.splitlines())
 
 steps = []
-steps_text = steps_text.strip()
 while steps_text:
 	r = parse("{:d}{}", steps_text)
 	if not r:
@@ -49,13 +42,14 @@ class Cell (object):
 		self.x = x
 		self.y = y
 		self.is_wall = c == "#"
-		self.up = None
+		self.up = None # once set, these are a tuple of (cell, # of clockwise rotations of direction to make after moving)
 		self.down = None
 		self.left = None
 		self.right = None
 
 pts = [[(Cell(i,j,c) if (c != " ") else None) for (i,c) in enumerate(row)] for (j,row) in enumerate(grid)]
 
+# set up initial connections
 for j, row in enumerate(pts):
 	first = None
 	for i, cell in enumerate(row):
@@ -67,8 +61,8 @@ for j, row in enumerate(pts):
 		if not nxt:
 			nxt = first
 			first = None
-		nxt.left = cell
-		cell.right = nxt
+		nxt.left = (cell, 0)
+		cell.right = (nxt, 0)
 
 for i in range(W):
 	first = None
@@ -87,8 +81,94 @@ for i in range(W):
 			nxt = pts[(j+1)%H][i]
 			if not nxt:
 				nxt = first
-		nxt.up = cell
-		cell.down = nxt
+		nxt.up = (cell, 0)
+		cell.down = (nxt, 0)
+		
+"""
+Square labeling in test data:
+	
+	A
+BCD
+  EF
+
+boundaries are AB, AC, AF, BE, BF, CE, DF
+"""
+
+# AB
+jA = 0
+jB = N
+for iA in range(2*N, 3*N):
+	iB = 3*N - 1 - iA
+	cellA = pts[jA][iA]
+	cellB = pts[jB][iB]
+	cellA.up = (cellB, 2)
+	cellB.up = (cellA, 2)
+
+# AC
+iA = 2*N
+jC = N
+for jA in range(N):
+	iC = N + jA
+	cellA = pts[jA][iA]
+	cellC = pts[jC][iC]
+	cellA.left = (cellC, 3)
+	cellC.up = (cellA, 1)
+
+# AF
+iA = 3*N-1
+iF = 4*N-1
+for jA in range(N):
+	jF = 3*N - 1 - jA
+	cellA = pts[jA][iA]
+	cellF = pts[jF][iF]
+	cellA.right = (cellF, 2)
+	cellF.right = (cellA, 2)
+
+# BE
+jB = 2*N-1
+jE = 3*N-1
+for iB in range(N):
+	iE = 3*N - 1 - iB
+	cellB = pts[jB][iB]
+	cellE = pts[jE][iE]
+	cellB.down = (cellE, 2)
+	cellE.down = (cellB, 2)
+
+# BF
+iB = 0
+jF = 3*N-1
+for jB in range(N, 2*N):
+	iF = 5*N - 1 - jB
+	cellB = pts[jB][iB]
+	cellF = pts[jF][iF]
+	cellB.left = (cellF, 1)
+	cellF.down = (cellB, 3)
+
+# CE
+jC = 2*N-1
+iE = 2*N
+for iC in range(N, 2*N):
+	jE = 4*N - 1 - iC
+	cellC = pts[jC][iC]
+	cellE = pts[jE][iE]
+	cellC.down = (cellE, 3)
+	cellE.left = (cellC, 1)
+
+# DF
+iD = 3*N-1
+jF = 2*N
+for jD in range(N, 2*N):
+	iF = 5*N - 1 - jD
+	cellD = pts[jD][iD]
+	cellF = pts[jF][iF]
+	cellD.right = (cellF, 1)
+	cellF.up = (cellD, 3)
+
+	
+rot_map = {
+		"R": {"R": "D", "D": "L", "L": "U", "U": "R"},
+		"L": {"R": "U", "U": "L", "L": "D", "D": "R"}
+	}
 
 def do_step(cur, step):
 	n, turn = step
@@ -100,15 +180,14 @@ def do_step(cur, step):
 		"D": lambda c: c.down
 	}
 	for _ in range(n):
-		next_cell = nxt[dir](cell)
+		next_cell, rots = nxt[dir](cell)
 		if next_cell.is_wall:
 			break
 		cell = next_cell
+		for _ in range(rots):
+			dir = rot_map["R"][dir]
 		
-	new_dir = {
-		"R": {"R": "D", "D": "L", "L": "U", "U": "R"},
-		"L": {"R": "U", "U": "L", "L": "D", "D": "R"}
-	}[turn][dir]
+	new_dir = rot_map[turn][dir]
 	return cell, new_dir
 
 # find upper left cell
@@ -124,4 +203,5 @@ for step in steps:
 	
 score = 1000*(cell.y+1) + 4*(cell.x+1) + {"R":0, "D": 1, "L": 2, "U": 3}[dir]
 
-print("Part 1:", score)
+print("Part 2 test data:", score)
+
