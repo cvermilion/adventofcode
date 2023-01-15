ce817ca57c5c0e0592eb992687f99abe87577833
Christopher Vermilion <christopher.vermilion@gmail.com>
2023-01-15 15:54:51 -0500

    clean up day 17

--- 2022/17/run.py
+++ 2022/17/run.py
@@ -1,4 +1,12 @@
+import os, sys
+sys.path.append(os.path.realpath(".."))
+from util import *
+from queue import PriorityQueue
+from collections import namedtuple
 from copy import deepcopy
+
+#test()
+input = get_input()
 
 pieces = [
 	
@@ -22,18 +30,13 @@ pieces = [
 	
 	]
 
-input_test = ">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>"
-#input = input_test
-input = open("input.py").read().strip()
+moves = input.strip()
 
-#chamber = [[1]*7] + [[0]*7 for _ in range(7)]
 chamber = [[0]*7 for _ in range(8)]
 
 def print_chamber(ch):
 	print("\n".join("|"+"".join("@" if c else "." for c in row)+"|" for row in reversed(ch)))
 	print("+-------+")
-
-print_chamber(chamber)
 
 def top_row(ch):
 	for i, row in enumerate(ch):
@@ -50,8 +53,6 @@ def print_chamber_with(ch, p, offset):
 	print("\n".join("|"+"".join("@" if c or (i,j) in pts else "." for (i,c) in enumerate(row))+"|" for (j, row) in reversed(list(enumerate(ch)))))
 	print("+-------+")
 
-#print_chamber_with(chamber, pieces[0], (1,3))
-
 def has_overlap(ch, p, offset):
 	pts = points(p, offset)
 	return any(ch[j][i] for (i,j) in pts)
@@ -59,13 +60,6 @@ def has_overlap(ch, p, offset):
 def add_piece(ch, p, offset):
 	for (i,j) in points(p, offset):
 		ch[j][i] = 1
-
-#add_piece(chamber, pieces[0], (1, 3))
-#print_chamber(chamber)
-
-#print(has_overlap(chamber, pieces[1], (2, 3)))
-
-moves = input.strip()
 
 def drop_piece(ch, p, move_idx):
 	top = top_row(ch)
@@ -92,16 +86,6 @@ def drop_piece(ch, p, move_idx):
 			return (move_idx, (offset[0], offset[1]-top))
 		offset = new_offset
 
-"""
-drop_piece(chamber, pieces[0], 0)
-print()
-print_chamber(chamber)
-
-add_piece(chamber, pieces[1], (2, top_row(chamber)+4))
-print()
-print_chamber(chamber)
-"""
-
 # part 1
 
 move_idx = 0
@@ -113,12 +97,7 @@ while total < 2022:
 	pieces_idx %= len(pieces)
 	total += 1
 
-#print_chamber(chamber)
-print(top_row(chamber)+1)
-
-
-
-# part 2
+print("Part 1:", top_row(chamber)+1)
 
 def top_row_for_col(ch, i):
 	for j in range(len(ch)-1, -1, -1):
@@ -146,20 +125,20 @@ def height_after(n_pieces):
 		total += 1
 		
 		if total % 1000 == 0:
-			print(total)
+			#print(total)
 			
 			j = min_top_row(chamber)
 			height_offset += j
 			chamber = chamber[j:]
 		
-		if total % move_period == 0:
-			print(total//move_period, "cycles")
+		#if total % move_period == 0:
+		#	print(total//move_period, "cycles")
 	
 	return height_offset + top_row(chamber) + 1
 
 move_period = len(moves) * P
 
-def find_period():
+def find_period_bad():
 	# this is wrong!! same offsets on different substrate dont nec give the same pattern
 	chamber = [[0]*7 for _ in range(8)]
 	move_idx = 0
@@ -195,7 +174,7 @@ def find_period():
 	
 	return None
 
-def find_period2(min, max):
+def find_period(min, max):
 	chamber = [[0]*7 for _ in range(8)]
 	move_idx = 0
 	total = 0
@@ -210,13 +189,12 @@ def find_period2(min, max):
 		pieces_idx += 1
 		pieces_idx %= P
 		total += 1
-		#history.append(offset)
 		
-		if total % 1000 == 0:
-			print(total)
+		#if total % 1000 == 0:
+		#	print(total)
 		
-		if total % move_period == 0:
-			print(total//move_period, "cycles")
+		#if total % move_period == 0:
+		#	print(total//move_period, "cycles")
 		
 		if total >= 1000:
 			j = top_row(chamber)
@@ -226,35 +204,11 @@ def find_period2(min, max):
 				for period in range(min, total-1000):
 					if top[total] == top[total-period]:
 						return period
-	"""
-	for period in range(min, max+1):
-		# Assume that we're cycling by 1000
-		if chamber[1000:1020] == chamber[1000+period:1020+period] and chamber[1000:1020] == chamber[1000+2*period:1020+2*period]:
-			return period
-	"""
 		
 	return None
-	
-
-#print(height_after(552 + 8*move_period + 63248))
-print(find_period2(1000, 5000))
-
-"""
-n, steps, top = find_period()
-print("period", n, steps, top)
-period = n*move_period
-intro = steps - period
-goal = 1000000000000
-n_periods = goal // period
-extra = goal % period
-
-delta = top - height_after(intro)
-full = height_after(extra) + n_periods*delta
-print(full)
-"""
 
 # lazy guess: period is between 1-5k
-period = find_period2(1000, 5000)
+period = find_period(1000, 5000)
 start = height_after(1000)
 end = height_after(1000+period)
 delta = end-start
@@ -263,23 +217,4 @@ n_periods = (goal-1000) // period
 extra = (goal-1000) % period
 rem = height_after(1000 + period + extra) - end
 full = start + n_periods*delta + rem
-print(full)
-
-# too high: 1524923198852
-
-# period 8 at 404192, h 616244
-# 552 + 8 periods
-# goal is 2477455*8p + 63800
-# or 552 + 2477455*8p + 63248
-# total: height at 63800 + 2477455*(delta for one period)
-# height(552) = 830
-# delta = 615414
-# height(63800) 97267
-# res = 1524660588637 : wrong
-
-# height 552 + 8p + 63248 = 712673
-# total = height at 552 + 2477455*delta + (height(552+8p+63248) - delta - height(552))
-# res = 1524660588629 : wrong
-
-# better test: cycle is 1725
-# res = 1524637681145 : correct
+print("Part 2:", full)
