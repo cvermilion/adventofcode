4c3da901f8dd9755addf6934a42d7d2756157bb6
Christopher Vermilion <christopher.vermilion@gmail.com>
2023-01-15 15:12:02 -0500

    clean up day 06

--- 2022/06/run.py
+++ 2022/06/run.py
@@ -1,11 +1,13 @@
-input_test = "mjqjpqmgbljsphdztnvjfqwrcgsml"
+import os, sys
+sys.path.append(os.path.realpath(".."))
+from util import *
 
-input = input_test
-input = open("input.py").read()
+#test()
+input = get_input()
 
-for N in (4,14):
+for part, N in enumerate([4,14]):
 	for i in range(N, len(input)):
 		if len(set(input[i-N:i])) == N:
 			break
-	print(i)
+	print("Part {:d}: {:d}".format(part+1, i))
 
