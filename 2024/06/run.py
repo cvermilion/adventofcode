0cf3f8b16a53565151dea62dfe077b787ae11d10
Christopher Vermilion <christopher.vermilion@gmail.com>
2024-12-07 16:55:55 -0500

    day 6+7 import

---
+++ 2024/06/run.py
@@ -0,0 +1,175 @@
+from me import *
+
+input = get_data_2024(6)
+
+#input = input_test
+
+class Point(tuple):
+	def __new__(cls,x,y):
+		self = super().__new__(cls,[x,y])
+		return self
+
+p = Point(1,2)
+
+PointT = namedtuple("Point", "x y")
+VecT = namedtuple("Vec", "x y")
+
+class Vec(VecT):
+	def ortho(self):
+		return self.x == 0 or self.y == 0
+	
+	def mag(self):
+		return abs(self.x) + abs(self.y)
+	
+	def __add__(self, v2):
+		return Vec(self.x+v2.x, self.y+v2.y)
+		
+	def __div__(self, v2):
+		# only implemented for normal vectors
+		if not self.ortho() or not v2.ortho():
+			return None
+		return None
+
+N,E,S,W = Vec(0,-1), Vec(1,0), Vec(0,1), Vec(-1,0)
+
+Path = namedtuple("Path", "dir mag")
+	
+class Point(PointT):
+	def __sub__(self, p2):
+		# None if not in the same row or column
+		if self.x == p2.x:
+			if self.y > p2.y:
+				return Path(S, self.y-p2.y)
+			else:
+				return Path(N, p2.y-self.y)
+		elif self.y == p2.y:
+			if self.x > p2.x:
+				return Path(E, self.x-p2.x)
+			else:
+				return Path(W, p2.x-self.x)
+		return None
+	
+	def __add__(self, path):
+		return Point(self.x + path.dir.x*path.mag, self.y + path.dir.y*path.mag)
+
+Cell = namedtuple("Cell", "point value")
+
+class Grid(object):
+	def __init__(self, grid):
+		Ni = len(grid[0])
+		Nj = len(grid)
+		self.cells = [[Cell(Point(i,j), val) for (i,val) in enumerate(row)] for (j,row) in enumerate(grid)]
+	
+	def __iter__(self):
+		for row in self.cells:
+			for c in row:
+				yield c
+
+# Part 1
+
+grid = lmap(list, input.strip().split("\n"))
+
+def rep_grid(g):
+    return "\n".join("".join(row) for row in g)
+
+def padded(N, c, grid):
+	gx = len(grid[0])
+	return (
+		[[c] * (gx+2*N)] * N
+		+ [[c]*N + row + [c]*N for row in grid]
+		+ [[c] * (gx+2*N)] * N
+	)
+
+Ni = len(grid[0])
+Nj = len(grid)
+#grid = padded(1, "0",  grid)
+
+print(rep_grid(grid))
+
+g = Grid(grid)
+
+start, start_dir = None, None
+obstacles = set()
+boundary = set()
+for c in g:
+	if c.value in "<>^v":
+		start = c.point
+		start_dir = {"v": S, "^": N, ">": E, "<": W}[c.value]
+	if c.value == "#":
+		obstacles.add(c.point)
+	if c.value == "0":
+		boundary.add(c.point)
+
+visited = set([start])
+cur, dir = start, start_dir
+print("start",start,dir)
+step=0
+while True:
+	step += 1
+	closest = None
+	for obs in obstacles:
+		path = obs-cur
+		if not path or path.dir != dir:
+			continue
+		if not closest or path.mag < closest.mag:
+			closest = path
+	
+	if closest:
+		# go one short of the obstacle and turn
+		next = cur + Path(closest.dir, closest.mag-1)
+		visited.update(set(cur + Path(closest.dir, m) for m in range(1, closest.mag)))
+		cur = next
+		dir = {N:E,E:S,S:W,W:N}[dir]
+		#print("stopping at", cur, dir)
+	else:
+		# hit a boundary
+		if dir == E:
+			visited.update(set(Point(i, cur.y) for i in range(cur.x+1, Ni)))
+		elif dir == S:
+			visited.update(set(Point(cur.x, j) for j in range(cur.y+1, Nj)))
+		elif dir == W:
+			visited.update(set(Point(i, cur.y) for i in range(cur.x)))
+		elif dir == N:
+			visited.update(set(Point(cur.x, j) for j in range(cur.y)))
+		break
+
+result1 = len(visited)
+
+print("Part 1:", result1)
+#aocd.submit(result1, part="a", day=6)
+
+# Part 2
+
+def has_loop(obstacle):
+	cur, dir = start, start_dir
+	visited = set([(start, start_dir)])
+	new_obstacles = obstacles.union(set([obstacle]))
+	while True:
+		closest = None
+		for obs in new_obstacles:
+			path = obs-cur
+			if not path or path.dir != dir:
+				continue
+			if not closest or path.mag < closest.mag:
+				closest = path
+		
+		if closest:
+			# go one short of the obstacle and turn
+			next = cur + Path(closest.dir, closest.mag-1)
+			#visited.update(set(cur + Path(closest.dir, m) for m in range(1, closest.mag)))
+			cur = next
+			dir = {N:E,E:S,S:W,W:N}[dir]
+			#print("stopping at", cur, dir)
+			if (cur, dir) in visited:
+				return True
+			visited.add((cur, dir))
+		else:
+			# escape
+			return False 
+
+to_check = visited
+to_check.remove(start)
+
+result2 = sum(1 for o in to_check if has_loop(o))
+print("Part 2:", result2)
+#aocd.submit(result2, part="b", day=6)
---
+++ 2024/07/run.py
@@ -0,0 +1,44 @@
+from me import *
+
+input = get_data_2024(7)
+
+#input = input_test
+
+def parse_line(l):
+	total, rest = l.strip().split(": ")
+	nums = rest.split(" ")
+	return int(total), lmap(int, nums)
+
+lines = lmap(parse_line, input.splitlines())
+
+# Part 1
+
+def check(total, nums):
+	if len(nums) == 1:
+		return total == nums[0]
+	if nums[0] > total:
+		# none of the operators makes the total smaller
+		return False
+	return check(total, [nums[0]+nums[1]] + nums[2:]) or check(total, [nums[0]*nums[1]] + nums[2:])
+	
+
+result1 = sum(total for (total, nums) in lines if check(total, nums))
+
+print("Part 1:", result1)
+#aocd.submit(result1, part="a", day=7)
+
+# Part 2
+
+def check2(total, nums):
+	if len(nums) == 1:
+		return total == nums[0]
+	if nums[0] > total:
+		# none of the operators makes the total smaller
+		return False
+	return check2(total, [nums[0]+nums[1]] + nums[2:]) or check2(total, [nums[0]*nums[1]] + nums[2:]) or check2(total, [int(str(nums[0])+str(nums[1]))] + nums[2:])
+	
+
+result2 = sum(total for (total, nums) in lines if check2(total, nums))
+
+print("Part 2:", result2)
+#aocd.submit(result2, part="b", day=7)
