9b18e590499c2b526fee6a2e277502c2a944895c
Christopher Vermilion <christopher.vermilion@gmail.com>
2025-12-03 11:51:55 -0500

    simplify day 3

--- 2025/03/run.py
+++ 2025/03/run.py
@@ -3,28 +3,10 @@ from sickos.yes import *
 
 DAY=3
 
-input = get_data_2025(DAY)
-
+#input = get_data_2025(DAY)
 input = input_test
 
-# Part 1
-
-banks = (pipeline(input.splitlines())
-	| [[int]]
-	| DONE)
-
-def max_joltage(bank):
-	first = bank.index(max(bank[:-1]))
-	second = first + 1 + bank[first+1:].index(max(bank[first+1:]))
-	return 10*bank[first] + bank[second]
-
-
-result1 = sum(map(max_joltage, banks))
-
-print("Part 1:", result1)
-#aocd.submit(result1, part="a", day=DAY)
-
-# Part 2
+banks = (pipeline(input.splitlines()) | [[int]] | DONE)
 
 # max using n digits
 def max_joltage(n, bank):
@@ -34,7 +16,14 @@ def max_joltage(n, bank):
 	first = bank.index(max(usable))
 	return bank[first]*10**(n-1) + max_joltage(n-1, bank[first+1:])
 
-result2 = sum(max_joltage(12, b) for b in banks)
+# Part 1
 
+result1 = sum(max_joltage(2, b) for b in banks)
+print("Part 1:", result1)
+#aocd.submit(result1, part="a", day=DAY)
+
+# Part 2
+
+result2 = sum(max_joltage(12, b) for b in banks)
 print("Part 2:", result2)
 #aocd.submit(result2, part="b", day=DAY)
